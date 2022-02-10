5e696a24134d50fed010cd1eac86ab915497e353
Nofar Vered Nahum <74105252+NofarVered@users.noreply.github.com>
2022-02-10 18:15:06 +0200

    Update main.py

--- SRC/QUERIES/main.py
+++ SRC/QUERIES/main.py
@@ -1,15 +1,1 @@
-import pymysql
 
-CONNECTOR = pymysql.connect(
-    host='localhost',
-    port=3305,
-    user='DbMysql36',
-    password='DbMysql36',
-    database='DbMysql36'
-)
-
-what_to_predict = """
-Welcome to .....
-"""
-
-navigate = input(what_to_predict)
