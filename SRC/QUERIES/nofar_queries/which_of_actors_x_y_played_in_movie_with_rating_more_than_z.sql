516ffdec75ffd3d17f483fdf5fb99215674cafe8
NofarVered <nofarss79@gmail.com>
2022-02-12 03:14:47 +0200

    some comments2

--- SRC/QUERIES/nofar_queries/which_of_actors_x_y_played_in_movie_with_rating_more_than_z.sql
+++ SRC/QUERIES/nofar_queries/which_of_actors_x_y_played_in_movie_with_rating_more_than_z.sql
@@ -1,9 +1,9 @@
-SELECT "Neither" AS option0,
-       (SELECT actors.name FROM actors WHERE actors.rnd_token = {actor_token1}) AS option1,
+SELECT CONCAT("Which of these two actors played in a movie that was rated more than %s on imdb?") AS question,
+"Neither" AS option0,
+       (SELECT actors.name FROM Actors WHERE actors.rnd_token = {actor_token1}) AS option1,
        "Both" AS option2,
        (SELECT actors.name FROM actors WHERE actors.rnd_token = {actor_token2}) AS option3,
        IF(COUNT(actors.rnd_token) <> 1, COUNT(actors.rnd_token), IF(MIN(actors.rnd_token) = {actor_token1}, 1, 3)) AS answer,
-       CONCAT("Which of these actors played in a movie that was rated more than ", {rating_token}, " on imdb?") AS question
 FROM actors
 WHERE (actors.rnd_token = {actor_token1} OR actors.rnd_token = {actor_token2}) AND
 	  EXISTS (SELECT movies.vote_average
