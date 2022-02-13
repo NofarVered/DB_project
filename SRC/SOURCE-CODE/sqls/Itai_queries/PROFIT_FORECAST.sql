b89e6c026b40b101df1b02f0b9205a22d28339f2
Itai Zemah <itaizemah@mail.tau.ac.il>
2022-02-13 16:47:39 +0200

    Update the query
    A query to fetch the delta between the specified year's revenue of a genre to the revenue of the year before that

--- SRC/SOURCE-CODE/sqls/Itai_queries/PROFIT_FORECAST.sql
+++ SRC/SOURCE-CODE/sqls/Itai_queries/PROFIT_FORECAST.sql
@@ -1,5 +1,11 @@
-SELECT  Yearly_revenue.YEAR,
+
+SELECT Q.Genre_name,  Q.revenues- Q. past_year_revenue AS Yearly_change
+FROM 
+(SELECT  Genres.name AS Genre_name, Yearly_revenue.YEAR,
         revenues,
-        LEAD (revenues, 12) OVER (ORDER BY month ASC) AS next_year_revenue
-FROM Yearly_revenue
-ORDER BY next_year_revenue DESC;
+        LAG (revenues) OVER (ORDER BY YEAR) AS past_year_revenue
+FROM Yearly_revenue, Genres
+WHERE Yearly_revenue.YEAR= "%s"
+AND Genres.name= "%s") AS Q
+
+-- A query to fetch the delta between the specified year's revenue of a genre to the revenue of the year before that
