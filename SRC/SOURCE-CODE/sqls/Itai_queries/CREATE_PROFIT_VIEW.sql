c898942ce44e50768236993e840a664e03932bb5
Itai Zemah <itaizemah@mail.tau.ac.il>
2022-02-13 16:18:02 +0200

    Merged into create schema
    Merged into create schema

--- SRC/SOURCE-CODE/sqls/Itai_queries/CREATE_PROFIT_VIEW.sql
+++
@@ -1,9 +0,0 @@
--- Calculate the yearly revenue of companies 
-CREATE VIEW Yearly_Revenues AS 
-SELECT Genres.name, YEAR(x.release_date) AS 'Work Year', SUM(y.profit) AS 'Revenues'
-FROM Movies x, Movies y, Movie_genres
-WHERE Movie_genres.genre_id= Genres.id AND x.id= Movie_genres.movie_id
-AND x.id= y.id
-GROUP BY Genres.name, 'Work Year'
-ORDER BY Work Year DESC;
-
