6935b6f9d84e95d92c0812b5b1f30edf0f65df9d
NofarVered <nofarss79@gmail.com>
2022-02-13 22:57:43 +0200

    22:54

--- SRC/SOURCE-CODE/sqls/How_many_X_actors_played_in_the_top_ten_movie_by_profit_3.sql
+++ SRC/SOURCE-CODE/sqls/How_many_X_actors_played_in_the_top_ten_movie_by_profit_3.sql
@@ -8,7 +8,7 @@ FROM
             Movies ON Movies.imdb_id = Movie_actors.movie_id
         INNER JOIN
         Actors ON Actors.imdb_id = Movie_actors.actor_id
-    WHERE Actors.sex= %s
+    WHERE Actors.sex= {pt}
     GROUP BY id
     ORDER BY profit DESC
     LIMIT 10) topten
