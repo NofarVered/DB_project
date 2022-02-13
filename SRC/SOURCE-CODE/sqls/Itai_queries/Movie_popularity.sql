442f0253994cec0b2b6dafdd4c3242149cf78257
Itai Zemah <itaizemah@mail.tau.ac.il>
2022-02-13 16:56:21 +0200

    Update and rename Movie_popularity.sql to Actor_genre_frequency.sql
    -- Show the most profitable actor that playes in at least %s movies of the specified genre

--- SRC/SOURCE-CODE/sqls/Itai_queries/Movie_popularity.sql
+++
@@ -1,8 +0,0 @@
--- Select all the companies from countries with at least 5 different movie companies, and show their boxoffice average 
-
-SELECT Companies.name, AVG(Movies.BoxOffice), Companies.origin_country
-FROM Companies, Movies, Movie_companies
-WHERE Companies.id= Movie_companies.company_id AND Movies.id= Movie_companies.movie_id
-AND Comapnies.origin_country IN
- (SELECT Companies.origin_country FROM Companies GROUP BY Companies.origin_country HAVING COUNT(*)>5)
-GROUP BY Companies.name
