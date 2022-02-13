cdbe8174c8e4451a85b15dbe1b4f5b00df43c585
NofarVered <nofarss79@gmail.com>
2022-02-13 21:28:52 +0200

    21:28

--- SRC/SOURCE-CODE/sqls/Actor_genre_X_frequency_7.sql
+++ SRC/SOURCE-CODE/sqls/Actor_genre_X_frequency_7.sql
@@ -2,10 +2,10 @@
 SELECT Actors.name
 FROM Actors, Genres, Movie_genres, Movie_actors
 WHERE Actors.id= Movie_actors.actor_id 
-AND Movie_actors.movie_id= Movies. id 
-AND Movies.id= Movie_genres.movie_id 
-AND genres.id=Movie_genres.genre_id
-AND genre.name = %s
+    AND Movie_actors.movie_id= Movies. id 
+    AND Movies.id= Movie_genres.movie_id 
+    AND genres.id=Movie_genres.genre_id
+    AND genre.name = %s
 GROUP BY Actors.name
-HAVING COUNT(DISTINCT Movies.id)> %s
+HAVING COUNT(DISTINCT Movies.id) => %s
 END
