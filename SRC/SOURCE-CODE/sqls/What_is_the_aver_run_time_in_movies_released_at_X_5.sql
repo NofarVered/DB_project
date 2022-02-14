f35eb71e1ed104a8faede87e4345fff898aac301
Itai Zemah <itaizemah@mail.tau.ac.il>
2022-02-14 18:39:47 +0200

    Final 18:39

--- SRC/SOURCE-CODE/sqls/What_is_the_aver_run_time_in_movies_released_at_X_5.sql
+++ SRC/SOURCE-CODE/sqls/What_is_the_aver_run_time_in_movies_released_at_X_5.sql
@@ -1,8 +1,15 @@
-SELECT CONCAT(" What is the average run time in movies released at {pt} (year)? ") AS question,
-AVG(a.run_time) as answer,
-FROM (SELECT Movies.run_time, Movies.imdb_id
-		FROM Movies
-		WHERE YEAR(Movies.release_date) = {pt}) a
-ORDER BY cnt DESC
-LIMIT 1
-END
+SELECT CONCAT(" What is the average run time in movies from the most profitable genre at {pt} (year)? ") AS question,
+AVG(a.run_time) as answer
+FROM (SELECT Movies.run_time, Movies.imdb_id, Genres.name 
+		FROM Movies, Genres, Movie_genres, Genre_Yearly_Revenues
+		WHERE YEAR(Movies.release_date) =2008
+        AND Movie_genres.movie_id= Movies.imdb_id
+        AND Movie_genres.genre_id= Genres.id
+        AND Genre_Yearly_Revenues.Year= 2008
+        AND Genre_Yearly_Revenues.name= Genres.name
+        AND Genre_Yearly_Revenues.Revenues>= ALL (SELECT Genre_Yearly_Revenues.Revenues 
+                                             FROM Genre_Yearly_Revenues
+                                             WHERE Year=2008)) AS a
+        
+ORDER BY answer DESC
+LIMIT 1;
