35268cf01cfc351749e97ca9d88aa12b2306054a
Itai Zemah <itaizemah@mail.tau.ac.il>
2022-02-14 18:07:29 +0200

    Update yearly_change_6.sql

--- SRC/SOURCE-CODE/sqls/yearly_change_6.sql
+++ SRC/SOURCE-CODE/sqls/yearly_change_6.sql
@@ -1,14 +1,15 @@
-SELECT Q.Genre_name,  Q.revenues- Q. past_year_revenue AS Yearly_change
-FROM 
-(SELECT  Genres.name AS Genre_name, Genre_Yearly_Revenues.Year,
-        Genre_Yearly_Revenues.revenues,
-        LAG (Genre_Yearly_Revenues.revenues) OVER (ORDER BY  Year) AS past_year_revenue
-FROM Genre_Yearly_Revenues, Genres
-WHERE Genre_Yearly_Revenues.Year= {user_year}
-AND Genres.name= {user_genre}
-AND past_year_revenue IS NOT NULL
+SELECT Q.Genre_name, Q.Year, Q.revenue- Q.past_year_revenue AS Yearly_change
+FROM
+(SELECT  Genres.name AS Genre_name, x.Year AS Year,
+        x.revenues AS revenue,
+        y.revenues AS past_year_revenue
+        FROM  Genres, Genre_Yearly_Revenues x, Genre_Yearly_Revenues y
+        WHERE x.Year= 2009
+        AND y.Year = (2009 - 1)
+        AND Genres.name= 'Action'
+        AND x.name= 'Action'
+        AND y.name= 'Action'
+        AND y.revenues IS NOT NULL
 ) AS Q
-LIMIT 1 
-END 
 
 -- A query to fetch the delta between the specified year's revenue of a genre to the revenue of the year before that
