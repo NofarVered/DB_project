7f77b50c4c3ab976f903b2af001d77b591a2e63e
Itai Zemah <itaizemah@mail.tau.ac.il>
2022-02-14 18:22:50 +0200

    Final 18:22

--- SRC/SOURCE-CODE/sqls/Actor_genre_X_frequency_7.sql
+++ SRC/SOURCE-CODE/sqls/Actor_genre_X_frequency_7.sql
@@ -1,11 +1,14 @@
 -- Show the most profitable actor that playes in at least X movies of the specified genre
-SELECT Actors.name
-FROM Actors, Genres, Movie_genres, Movie_actors
-WHERE Actors.id= Movie_actors.actor_id 
-    AND Movie_actors.movie_id= Movies. id 
-    AND Movies.id= Movie_genres.movie_id 
-    AND genres.id=Movie_genres.genre_id
-    AND genre.name = {user_genre}
+SELECT Q.Actor_name, Q.total_profit
+FROM
+(SELECT Actors.name AS Actor_name, Genres.name, SUM(Movies.profit) AS total_profit
+FROM Actors, Genres, Movie_genres, Movie_actors, Movies
+WHERE Actors.imdb_id= Movie_actors.actor_id 
+    AND Movie_actors.movie_id= Movies.imdb_id 
+    AND Movies.imdb_id= Movie_genres.movie_id 
+    AND Genres.id=Movie_genres.genre_id
+    AND Genres.name = 'Comedy'
 GROUP BY Actors.name
-HAVING COUNT(DISTINCT Movies.id) => {user_num}
-END
+HAVING COUNT(DISTINCT Movies.imdb_id) >=2) AS Q
+ORDER BY Q.total_profit DESC
+LIMIT 1;
